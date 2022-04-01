d232f2a41f1c4ec9222aef192d0db4ddd7acb469
Mahathi1992 <mahathiraga.air@gmail.com>
2022-04-01 14:30:40 +0200

    Initial commit for mapping, planning

---
+++ acl-gazebo/acl_sim/scripts/pubTrajRotorsSim.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+#Mahathi Bhargavapuri, Aug 2021
+
+#This generates a circle and publishes on the topic "goal" for the perfect tracker.
+
+#make sure to run this node AFTER roslaunch acl_sim perfect_tracker_and_sim.launch
+
+import roslib
+import rospy
+import math
+import trajectory_msgs.msg as trajmsg
+from geometry_msgs.msg import Pose
+from gazebo_msgs.msg import ModelState
+import numpy as np
+from numpy import linalg as LA
+
+from tf.transformations import quaternion_from_euler, euler_from_quaternion, quaternion_about_axis, quaternion_multiply
+
+from visualization_msgs.msg import Marker
+
+import tf
+from time import time
+
+##########################
+###### Functions #########
+##########################
+
+
+name = rospy.get_namespace() # has namespace with '/' at beginning and end of the name
+pubGoal = rospy.Publisher(name+'command/trajectory', trajmsg.JointTrajectory, queue_size=10)
+
+
+
+
+
+def startNode():
+    global target_position_x
+    global target_position_y
+    counter = -1
+    #global counter = -1
+    ##call circle_circumference
+    target_points = circle_circumference(0,0,20)
+    target_position_x = target_points[0][0]
+    target_position_y = target_points[0][1]
+
+    rate = rospy.Rate(5)
+    #rospy.loginfo("Starting...")
+    print("Starting MTB's test run ...")
+
+    desState = trajmsg.JointTrajectory()
+
+    while not rospy.is_shutdown():
+
+        if counter < len(target_points):
+            counter = counter + 1
+        if counter == len(target_points):
+            counter = 0
+        target_position_x = target_points[counter][0]
+        target_position_y = target_points[counter][1]
+
+        desState.p.x = target_position_x
+        desState.p.y = target_position_y
+        desState.p.z = 1.5
+        desState.v.x = 0
+        desState.v.y = 0
+        desState.v.z = 0
+        desState.a.x = 0
+        desState.a.y = 0
+        desState.a.z = 0
+        desState.yaw = 0
+
+        pubGoal.publish(desState)
+        #print(desState)
+        rate.sleep()
+
+    rospy.loginfo("Bye!")
+
+##########################
+##########################
+
+if __name__ == '__main__':
+
+    ns = rospy.get_namespace() # with '/' before and after name
+    try:
+        rospy.init_node('makeCircleTest')
+        if str(ns) == '/':
+            rospy.logfatal("Need to specify namespace as vehicle name.")
+            rospy.logfatal("This is typically accomplished in a launch file.")
+        else:
+            print ("Starting make circle node for: " + ns)
+            startNode()
+    except rospy.ROSInterruptException:
+        pass
